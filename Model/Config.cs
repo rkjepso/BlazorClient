84fdf2622c922b76c27b5a0ab3d5a5217db86421
rkjepso <38100136+rkjepso@users.noreply.github.com>
2021-04-24 17:45:52 +0200

    Cleanup

---
+++ Model/Config.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Model
+{
+    public enum LangToFrom { EngSpa, SpaEng, NorSpa, SpaNor };
+    public enum Order { Sequental, Random, Artif_Int };
+    public enum Auto { Auto500ms = 500, Auto1s = 1000, Auto2s = 2000, Auto3s = 3000, Auto5s = 5000, Manual = 0 };
+    public enum Mode { Scrolling, OneByOne, All_Time, All_Now, All_Man };
+
+    // State variables 
+    [Serializable]
+    public struct Data
+    {
+        public Mode Mode { get; set; }
+        public LangToFrom LangToFrom { get; set; }
+
+        public Order Order { get; set; }
+        public Auto Auto { get; set; }
+        public int SecThink { get; set; }
+        public int NumBatch { get; set; }
+        public int TotalWords { get; set; }
+        public int IdxStart { get; set; }
+        public int Step { get; set; }
+
+        public void Default()
+        {
+            Mode = (Mode.OneByOne);
+            LangToFrom = (LangToFrom.SpaNor);
+            Order = (Order.Sequental);
+            Auto = (Auto.Auto1s);
+            SecThink = 2000;
+            NumBatch = 3;
+            TotalWords = 1000;
+            IdxStart = 0;
+            Step = 25;
+        }
+
+        public void DefaultsForTest()
+        {
+            Mode = (Mode.OneByOne);
+            Order = (Order.Sequental);
+            NumBatch = 5;
+            SecThink = 2000;
+        }
+
+    }
+    public class Config
+    {
+    }
+}
--- Pages/Gloser.cs
+++ Pages/Gloser.cs
@@ -6,74 +6,45 @@ using System.Threading.Tasks;
 using BlazorGloser.Services;
 using Microsoft.AspNetCore.Components;
 
-
+using BlazorClient.Model;
 namespace BlazorClient.Pages
 {
-    public enum LangToFrom  { EngSpa, SpaEng, NorSpa, SpaNor};
-    public enum Order       { Sequental, Random, Artif_Int };
-    public enum Auto        { Auto500ms = 500, Auto1s = 1000, Auto2s = 2000, Auto3s = 3000, Auto5s = 5000, Manual = 0 };
-    public enum Mode        { Scrolling, OneByOne, All_Time, All_Now, All_Man };
-
-    // State variables 
-    [Serializable]
-    public struct Data
-    {
-        public Mode Mode       {get; set;}
-        public LangToFrom LangToFrom {get; set; } 
-
-        public Order Order     { get; set; }
-        public Auto Auto       { get; set; }
-        public int  SecThink   { get; set; } 
-        public int  NumBatch   { get; set; }
-        public int  TotalWords     { get; set; }
-        public int  IdxStart   { get; set; }
-        public int  Step { get; set; }
-
-        public void Default()
-        {
-            Mode = ( Mode.OneByOne);
-            LangToFrom = (LangToFrom.SpaNor);
-            Order = (Order.Sequental);
-            Auto = ( Auto.Auto1s);
-            SecThink = 2000;
-            NumBatch = 3;
-            TotalWords = 1000;
-            IdxStart = 0;
-        }
-
-    }
-
     public partial class Gloser 
     {
-        static public Data dt = new ();
-        static public Data GetData()
+        private static Data _dt = new();
+        private static Data _dtTest = new();
+        public  static Data Cfg 
         {
-            return dt;
+            get => GetGloser()?.state == "Testing" ? _dtTest : _dt;
         }
+
+        static public Data GetData() => _dt;
+  
         [Inject]
         protected ILocalStorageService Storage { get; set; }
 
-        public void SaveParams() => Storage.SetItem("__gloser", dt);
+        public void SaveParams() => Storage.SetItem("__gloser", _dt);
         public async Task LoadParams()
         {
             try
             {
-                dt = await Storage.GetItem<Data>("__gloser");
-                if (dt.Equals(default(Data)))
-                    dt.Default();
+                _dt = await Storage.GetItem<Data>("__gloser");
+                if (_dt.Equals(default(Data)))
+                    _dt.Default();
             }
             catch (Exception)
             {
-                dt.Default();
+                _dt.Default();
             }
-
+            _dtTest = _dt;
+            _dtTest.DefaultsForTest();
         }
 
         static string StrToLang
         {
             get
             {
-                return dt.LangToFrom switch
+                return Cfg.LangToFrom switch
                 {
                     LangToFrom.EngSpa => "Spanish",
                     LangToFrom.SpaEng => "English",
@@ -87,7 +58,7 @@ namespace BlazorClient.Pages
         {
             get
             {
-                return dt.LangToFrom switch
+                return Cfg.LangToFrom switch
                 {
                     LangToFrom.EngSpa => "English",
                     LangToFrom.SpaEng => "Spanish",
